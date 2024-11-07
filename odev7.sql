f926636fdae4a4d6a61c2e8eca08f83f4315ef72
aykanavsar <aykan.avsar@gmail.com>
2024-11-07 18:01:59 +0300

    Add files via upload

---
+++ odev7.sql
@@ -0,0 +1,18 @@
+--SELECT rating FROM film 
+--GROUP BY rating;
+
+--SELECT replacement_cost, COUNT(*) FROM film 
+--GROUP  BY replacement_cost
+--HAVING COUNT(*) > 50
+--ORDER BY replacement_cost
+
+--SELECT COUNT(store_id) FROM customer 
+
+--SELECT country_id, COUNT(*) AS  FROM city
+--GROUP BY country_id 
+
+--SELECT country_id, COUNT(*) AS city_count FROM city
+--GROUP BY country_id
+--ORDER BY city_count DESC
+--LIMIT 1;
+
