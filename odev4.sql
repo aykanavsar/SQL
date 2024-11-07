f81cc66cc35229463152fcbddba52653c03fad1b
aykanavsar <aykan.avsar@gmail.com>
2024-11-07 15:26:40 +0300

    Add files via upload

---
+++ odev4.sql
@@ -0,0 +1,5 @@
+--SELECT DISTINCT replacement_cost FROM film;
+--SELECT COUNT(DISTINCT replacement_cost) FROM film;
+--SELECT DISTINCT title FROM film WHERE title lIKE 'T%' AND rating = 'G';
+--SELECT DISTINCT country FROM country WHERE country lIKE '_____';
+--SELECT DISTINCT city FROM city WHERE city LIKE 'R%';
