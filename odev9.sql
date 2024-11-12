2f6068071b45a8088a08397bb3ac5dd576cd3e74
aykanavsar <aykan.avsar@gmail.com>
2024-11-12 10:19:21 +0300

    Add files via upload

---
+++ odev9.sql
@@ -0,0 +1,13 @@
+-- Ödev 1 
+--SELECT*FROM country
+--INNER JOIN city on country.country_id = city.country_id;
+
+--Ödev 2
+
+--SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer 
+--INNER JOIN payment ON  customer.customer_id = payment.customer_id ;
+
+
+--Ödev 3 
+--SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer 
+--INNER JOIN rental ON  customer.customer_id = rental.rental_id ;
